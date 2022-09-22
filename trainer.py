9c56945d4a8a648c1325ce79444a1838d40227f7
Amir Hadifar <amir.hadifar@ugent.be>
2022-09-22 11:14:34 +0200

    fix bug for cuda device on trainer

--- trainer.py
+++ trainer.py
@@ -9,6 +9,6 @@ class CustomTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get('logits')
         # compute custom loss
-        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor([1., 2.]))
+        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor([1., 2.], device=logits.get_device()))
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
